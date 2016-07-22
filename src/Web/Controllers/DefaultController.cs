6e3dd86fdc0c22aa28e711cc2519ab5084c9f32a
Ravi Mohan <ravi.mohan@roonga.com.au>
2016-07-22 20:41:00 +1000

    default routing

---
+++ src/Web/Controllers/DefaultController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("[controller]")]
+    public class DefaultController : Controller
+    {
+        [HttpGet]
+        public IActionResult Default()
+        {
+            return View();
+        }
+    }
+}
